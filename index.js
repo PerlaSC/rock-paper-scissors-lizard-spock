1507b8cd2c31f09d1e4cb59a31ef229c3b0e0666
Perla Sanchez <perlascabrera@gmail.com>
2020-07-28 17:24:06 -0500

    add event listener rules

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-const scissor = document.getElementById('scissor')
-const paper = document.getElementById('paper')
-const rock = document.getElementById('rock')
-const lizard = document.getElementById('lizard')
-const spock = document.getElementById('spock')
-const rules = document.getElementById('rules')
-const modal = document.getElementById('modal')
+const $scissor = document.getElementById('scissor')
+const $paper = document.getElementById('paper')
+const $rock = document.getElementById('rock')
+const $lizard = document.getElementById('lizard')
+const $spock = document.getElementById('spock')
+const $rules = document.getElementById('rules')
+const $modal = document.getElementById('modal')
+const $background = document.getElementById('background')
+const $close = document.getElementById('close')
 
+function modal(){
+    $modal.classList.toggle('modal')
+    $modal.classList.toggle('hidden')
+}
+function background(){
+    $background.classList.toggle('hidden')
+    $background.classList.toggle('background')
+}
 
-rules.addEventListener('click', ()=>{
-    modal.classList.add('modal')
-    modal.classList.remove('hidden')
+$rules.addEventListener('click', ()=>{
+    modal()
+    background()
+
+})
+$close.addEventListener('click', ()=>{
+    modal()
+    background()
 })
