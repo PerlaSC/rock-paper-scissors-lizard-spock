201c4d907a89116ffe51a6252e5a63b1e5e8b18e
Perla Sanchez <perlascabrera@gmail.com>
2020-07-28 16:47:57 -0500

    modal

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ const paper = document.getElementById('paper')
 const rock = document.getElementById('rock')
 const lizard = document.getElementById('lizard')
 const spock = document.getElementById('spock')
+const rules = document.getElementById('rules')
+const modal = document.getElementById('modal')
 
+
+rules.addEventListener('click', ()=>{
+    modal.classList.add('modal')
+    modal.classList.remove('hidden')
+})
