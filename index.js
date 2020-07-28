2d3fcdc45c6ea72510f824b827490a4fc8c7735b
Perla Sanchez <perlascabrera@gmail.com>
2020-07-28 16:02:25 -0500

    get Elements by Ids in JS

---
+++ index.js
@@ -0,0 +1,6 @@
+const scissor = document.getElementById('scissor')
+const paper = document.getElementById('paper')
+const rock = document.getElementById('rock')
+const lizard = document.getElementById('lizard')
+const spock = document.getElementById('spock')
+
