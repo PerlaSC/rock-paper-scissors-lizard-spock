fbec039ed8b7a428421fbe7a21206311338b2cc4
Perla Sanchez <perlascabrera@gmail.com>
2020-07-29 13:27:27 -0500

    JS code

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const $rules = document.getElementById('rules')
 const $modal = document.getElementById('modal')
 const $background = document.getElementById('background')
 const $close = document.getElementById('close')
+const $play = document.getElementById('play')
+const $game = document.getElementById('game')
+const $button = document.getElementById('button')
+const $resolve = document.getElementById('resolve')
 
 const number = Math.floor(Math.random()*5)
 
@@ -19,6 +23,15 @@ function background(){
     $background.classList.toggle('background')
 }
 
+function play(){
+    $play.classList.toggle('play')
+    $play.classList.toggle('hidden')
+    
+}
+function game(){
+    $game.classList.toggle('game')
+    $game.classList.toggle('hidden')
+}
 $rules.addEventListener('click', () => {
     modal()
     background()
@@ -28,3 +41,53 @@ $close.addEventListener('click', () => {
     modal()
     background()
 })
+
+$scissor.addEventListener('click', () => {
+    GamerPicked($scissor)
+    game()
+    play()
+    if (TransformNumber(0)){
+        return $play.appendChild($rock)
+    }
+})
+
+$button.addEventListener('click', () => {
+    game()
+    play()
+})
+
+function TransformNumber(number){
+    switch(number) {
+        case 0:
+            return $rock
+        case 1:
+            return $scissor
+        case 2: 
+            return $lizard
+        case 3:
+            return $paper
+        case 4:
+            return $spock
+    }
+}
+function TransformIcon(gamer){
+    switch(gamer){
+        case $rock:
+            return 0
+        case $scissor:
+            return 1
+        case $lizard: 
+            return 2
+        case $paper:
+            return 3
+        case $spock:
+            return 4
+    }
+}
+
+function GamerPicked(gamer){
+    return $play.appendChild(gamer)
+}
+function TheHousePicked(house){
+    r
+}
