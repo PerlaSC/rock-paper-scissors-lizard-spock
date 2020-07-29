75ec65ba8fca4d8f1157d71fb09e0394f7afb24f
Perla Sanchez <perlascabrera@gmail.com>
2020-07-29 15:05:22 -0500

    JS code

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const $scissor = document.getElementById('scissor')
+const $scissor = document.querySelector('.scissor')
 const $paper = document.getElementById('paper')
 const $rock = document.getElementById('rock')
 const $lizard = document.getElementById('lizard')
@@ -11,8 +11,20 @@ const $play = document.getElementById('play')
 const $game = document.getElementById('game')
 const $button = document.getElementById('button')
 const $resolve = document.getElementById('resolve')
+const $gamerPick = document.getElementById('gamerPick')
+const $housePick = document.getElementById('housePick')
+const $score = document.getElementById('score')
 
-const number = Math.floor(Math.random()*5)
+var img = ['icon-scissors.svg', 'icon-paper.svg', 'icon-rock.svg', 'icon-lizard.svg', 'icon-spock.svg']
+var title = [' You Win', 'You Lose', 'Draw']
+var iconClass = ['scissor', 'paper', 'rock', 'lizard', 'spock']
+
+var score = 0;
+
+function finalScore(){
+    score += 1
+    $score.innerHTML = `<p>${score}</p>`
+}
 
 function modal(){
     $modal.classList.toggle('modal')
@@ -43,12 +55,161 @@ $close.addEventListener('click', () => {
 })
 
 $scissor.addEventListener('click', () => {
-    GamerPicked($scissor)
+    var number = Math.floor(Math.random()*5)
+    GamerPicked(iconClass[0], img[0])
     game()
     play()
-    if (TransformNumber(0)){
-        return $play.appendChild($rock)
-    }
+    switch(number){
+        case 0: 
+            $housePick.innerHTML = `<article class="container ${iconClass[0]}"><img src="images/${img[0]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[2]}</h1>`;
+            break
+        case 1:
+            $housePick.innerHTML = `<article class="container ${iconClass[1]}"><img src="images/${img[1]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+        case 2:
+            $housePick.innerHTML = `<article class="container ${iconClass[2]}"><img src="images/${img[2]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+        case 3:
+            $housePick.innerHTML = `<article class="container ${iconClass[3]}"><img src="images/${img[3]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+        case 4:
+            $housePick.innerHTML = `<article class="container ${iconClass[4]}"><img src="images/${img[4]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+            
+    }    
+})
+
+$paper.addEventListener('click', () => {
+    var number = Math.floor(Math.random()*5)
+    GamerPicked(iconClass[1], img[1])
+    game()
+    play()
+    switch(number){
+        case 0: 
+            $housePick.innerHTML = `<article class="container ${iconClass[0]}"><img src="images/${img[0]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+        case 1:
+            $housePick.innerHTML = `<article class="container ${iconClass[1]}"><img src="images/${img[1]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[2]}</h1>`;
+            break
+        case 2:
+            $housePick.innerHTML = `<article class="container ${iconClass[2]}"><img src="images/${img[2]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+        case 3:
+            $housePick.innerHTML = `<article class="container ${iconClass[3]}"><img src="images/${img[3]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+        case 4:
+            $housePick.innerHTML = `<article class="container ${iconClass[4]}"><img src="images/${img[4]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+            
+    }    
+})
+
+$rock.addEventListener('click', () => {
+    var number = Math.floor(Math.random()*5)
+    GamerPicked(iconClass[2], img[2])
+    game()
+    play()
+    switch(number){
+        case 0: 
+            $housePick.innerHTML = `<article class="container ${iconClass[0]}"><img src="images/${img[0]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+        case 1:
+            $housePick.innerHTML = `<article class="container ${iconClass[1]}"><img src="images/${img[1]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+        case 2:
+            $housePick.innerHTML = `<article class="container ${iconClass[2]}"><img src="images/${img[2]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[2]}</h1>`;
+            break
+        case 3:
+            $housePick.innerHTML = `<article class="container ${iconClass[3]}"><img src="images/${img[3]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+        case 4:
+            $housePick.innerHTML = `<article class="container ${iconClass[4]}"><img src="images/${img[4]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+            
+    }    
+})
+$lizard.addEventListener('click', () => {
+    var number = Math.floor(Math.random()*5)
+    GamerPicked(iconClass[3], img[3])
+    game()
+    play()
+    switch(number){
+        case 0: 
+            $housePick.innerHTML = `<article class="container ${iconClass[0]}"><img src="images/${img[0]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+        case 1:
+            $housePick.innerHTML = `<article class="container ${iconClass[1]}"><img src="images/${img[1]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+        case 2:
+            $housePick.innerHTML = `<article class="container ${iconClass[2]}"><img src="images/${img[2]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+        case 3:
+            $housePick.innerHTML = `<article class="container ${iconClass[3]}"><img src="images/${img[3]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[2]}</h1>`;
+            break
+        case 4:
+            $housePick.innerHTML = `<article class="container ${iconClass[4]}"><img src="images/${img[4]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+            
+    }    
+})
+$spock.addEventListener('click', () => {
+    var number = Math.floor(Math.random()*5)
+    GamerPicked(iconClass[4], img[4])
+    game()
+    play()
+    switch(number){
+        case 0: 
+            $housePick.innerHTML = `<article class="container ${iconClass[0]}"><img src="images/${img[0]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+        case 1:
+            $housePick.innerHTML = `<article class="container ${iconClass[1]}"><img src="images/${img[1]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+        case 2:
+            $housePick.innerHTML = `<article class="container ${iconClass[2]}"><img src="images/${img[2]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[0]}</h1>`;
+            finalScore()
+            break
+        case 3:
+            $housePick.innerHTML = `<article class="container ${iconClass[3]}"><img src="images/${img[3]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[1]}</h1>`;
+            break
+        case 4:
+            $housePick.innerHTML = `<article class="container ${iconClass[4]}"><img src="images/${img[4]}"></article>`;
+            $resolve.innerHTML = `<h1>${title[2]}</h1>`;
+            break
+            
+    }    
 })
 
 $button.addEventListener('click', () => {
@@ -56,38 +217,7 @@ $button.addEventListener('click', () => {
     play()
 })
 
-function TransformNumber(number){
-    switch(number) {
-        case 0:
-            return $rock
-        case 1:
-            return $scissor
-        case 2: 
-            return $lizard
-        case 3:
-            return $paper
-        case 4:
-            return $spock
-    }
-}
-function TransformIcon(gamer){
-    switch(gamer){
-        case $rock:
-            return 0
-        case $scissor:
-            return 1
-        case $lizard: 
-            return 2
-        case $paper:
-            return 3
-        case $spock:
-            return 4
-    }
-}
 
-function GamerPicked(gamer){
-    return $play.appendChild(gamer)
-}
-function TheHousePicked(house){
-    r
+function GamerPicked(gamer, img){
+    return $gamerPick.innerHTML = `<article class="container ${gamer}"><img src="images/${img}"></article>`
 }
